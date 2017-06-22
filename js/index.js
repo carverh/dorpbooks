326b916c0dbf30a54f66186d96ccc0be0f5b4b72
Carver Harrison <pcarverb@hotmail.com>
2017-06-22 15:52:29 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,3 +2,22 @@
 Dorpbooks
 Copyright 2017 Carver Harrison
 */
+
+function $(s) {
+  return document.querySelector(s);
+}
+
+function $a(s) {
+  return document.querySelectorAll(s);
+}
+
+document.addEventListener('load', function() {
+  var ir = new XMLHttpRequest();
+  $a('include').forEach(function(i) {
+    ir.addEventListener("load", function() {
+      i.outerHTML = this.response
+    });
+    ir.open("GET", i.getAttribute('src'));
+    ir.send();
+  })
+})
